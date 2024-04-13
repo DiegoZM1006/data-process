8e04e66ddd3bba4dc2724d6ddeaad50c023b5210
Diego Zuñiga <1109184718@u.icesi.edu.co>
2024-04-13 11:45:12 -0500

    Update settings.py

--- ti/settings.py
+++ ti/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$cu#4r8g-k(ivnr1cp9fl#)%#=2a6hktz#u@q*h0uy(z7_6x2+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://integrativetask.azurewebsites.net/', 'http://127.0.0.1:8000/']
 
 
 # Application definition
